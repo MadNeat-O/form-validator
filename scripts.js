0d849ae8b8c67e82eadee8de9a2474267797b353
Brad Korman <brad.korman@remarque-creative.com>
2020-07-23 19:54:21 -0400

    added js validations

--- scripts.js
+++ scripts.js
@@ -18,6 +18,46 @@ const showSuccess = (input) => {
   formControl.className = 'form-control success'
 }
 
+const checkEmail = (input) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value)) {
+      showSuccess(input)
+    } else {
+      showError(input, 'Email is not valid')
+    }
+}
+
+const getFieldName = (input) => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+const checkRequired = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value === '') {
+      showError(input, `${getFieldName(input)} is required`)
+    }
+  })
+}
+
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+     showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  } else {
+    showSuccess(input )
+  }
+}
+
+const checkPasswordsMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  } else {
+    showSuccess(input1);
+    showSuccess(input2)
+  }
+}
+
 // Event Listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -26,4 +66,12 @@ form.addEventListener('submit', (e) => {
   } else {
     showSuccess(username)
   }
+})
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2)
 })
